2bf65fa927d0ea199f46092e6ebe8087cb9b0b15
unknown <you@example.com>
2023-02-19 16:37:15 +0800

    feat: Initial landing page design; Added new components

---
+++ resources/views/components/features-sample.blade.php
@@ -0,0 +1,74 @@
+{{-- features sample--}}
+<div class="max-w-full px-auto py-auto">
+    <div class="bg-white">
+        <div class="mx-auto grid max-w-2xl grid-cols-1 items-center gap-y-16 gap-x-8 py-24 px-4 sm:px-6 sm:py-32 lg:max-w-7xl lg:grid-cols-1 lg:px-8">
+            <div>
+                <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl ">Featured Properties</h2>
+                <p class="mt-4 text-gray-500">The walnut wood card tray is precision milled to perfectly fit a stack of
+                    Focus cards. The powder coated steel divider separates active cards from new ones, or can be used to
+                    archive important task lists.</p>
+            </div>
+            <div class="grid grid-cols-4 grid-rows-1 gap-4 sm:gap-6 lg:gap-8">
+                {{-- feature 1 --}}
+                <div class="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30 rounded-lg">
+                    <img src="/img/card1.png" alt="Feature 1"
+                        class="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125">
+
+                    {{-- feature desc --}}
+                    <div class="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
+                    <div class="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+                        <h1 class="font-dmserif text-3xl font-bold text-white">Feature</h1>
+                    </div>
+                </div>
+
+                {{-- feature 2 --}}
+                <div
+                    class="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30 rounded-lg">
+                    <img src="/img/card2.png" alt="Feature 1"
+                        class="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125">
+
+                    {{-- feature desc --}}
+                    <div
+                        class="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70">
+                    </div>
+                    <div
+                        class="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+                        <h1 class="font-dmserif text-3xl font-bold text-white">Feature</h1>
+                    </div>
+                </div>
+
+                {{-- feature 3 --}}
+                <div
+                    class="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30 rounded-lg">
+                    <img src="/img/card3.png" alt="Feature 1"
+                        class="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125">
+
+                    {{-- feature desc --}}
+                    <div
+                        class="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70">
+                    </div>
+                    <div
+                        class="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+                        <h1 class="font-dmserif text-3xl font-bold text-white">Feature</h1>
+                    </div>
+                </div>
+
+                {{-- feature 4 --}}
+                <div
+                    class="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30 rounded-lg">
+                    <img src="/img/card4.png" alt="Feature 1"
+                        class="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125">
+
+                    {{-- feature desc --}}
+                    <div
+                        class="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70">
+                    </div>
+                    <div
+                        class="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+                        <h1 class="font-dmserif text-3xl font-bold text-white">Feature</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,128 +1,200 @@
-<header class="bg-white shadow dark:bg-gray-900">
-    <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
-        <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-            <a href="https://flowbite.com" class="flex items-center">
-                <img src="https://flowbite.com/docs/images/logo.svg" class="mr-3 h-6 sm:h-9" alt="somelogo" />
-                <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">sometitle</span>
-            </a>
-            <div class="flex items-center lg:order-2">
-                @auth
-                    <form method="POST" action="{{ route('logout') }}" class="mb-0">
-                        @csrf
+<nav
+	class="fixed top-0 left-0 z-20 w-full border-b border-gray-200 bg-white px-2 py-2.5 dark:border-gray-600 dark:bg-gray-900 sm:px-4"
+>
+	<div class="container mx-auto flex flex-wrap items-center justify-between">
+		<a
+			class="flex items-center"
+			href="https://flowbite.com/"
+		>
+			<img
+				alt="sometitle"
+				class="mr-3 h-6 sm:h-9"
+				src="https://flowbite.com/docs/images/logo.svg"
+			/>
+			<span
+				class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
+			>sometitle</span>
+		</a>
+		<div class="flex md:order-2">
+			@auth
+				<form
+					action="{{ route('logout') }}"
+					class="mb-0"
+					method="POST"
+				>
+					@csrf
 
-                        <button id="dropdownAvatarNameButton" data-dropdown-toggle="dropdownAvatarName"
-                            class="flex items-center text-sm font-medium text-gray-900 rounded-full hover:text-blue-600 dark:hover:text-blue-500 md:mr-0 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:text-white"
-                            type="button">
-                            <span class="sr-only">Open user menu</span>
-                            <img class="w-8 h-8 mr-2 rounded-full"
-                                src="https://flowbite.com/docs/images/people/profile-picture-1.jpg" alt="user photo">
-                            {{ Str::title(Auth::user()->name . ' sagun') }}
-                            <svg class="w-4 h-4 mx-1.5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
-                                xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd"
-                                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                    clip-rule="evenodd"></path>
-                            </svg>
-                        </button>
+					<button
+						class="flex items-center rounded-full text-sm font-medium text-gray-900 hover:text-blue-600 focus:ring-4 focus:ring-gray-100 dark:text-white dark:hover:text-blue-500 dark:focus:ring-gray-700 md:mr-0"
+						data-dropdown-toggle="dropdownAvatarName"
+						id="dropdownAvatarNameButton"
+						type="button"
+					>
+						<span class="sr-only">Open user menu</span>
+						<img
+							alt="user photo"
+							class="mr-2 h-8 w-8 rounded-full"
+							src="https://flowbite.com/docs/images/people/profile-picture-1.jpg"
+						>
+						{{ Str::title(Auth::user()->name . ' sagun') }}
+						<svg
+							aria-hidden="true"
+							class="mx-1.5 h-4 w-4"
+							fill="currentColor"
+							viewBox="0 0 20 20"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<path
+								clip-rule="evenodd"
+								d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+								fill-rule="evenodd"
+							></path>
+						</svg>
+					</button>
 
-                        <!-- Dropdown menu -->
-                        <div id="dropdownAvatarName"
-                            class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
-                            <div class="px-4 py-3 text-sm text-gray-900 dark:text-white">
-                                <div class="font-medium ">Pro User</div>
-                                <div class="truncate">{{ Auth::user()->email }}</div>
-                            </div>
-                            <ul class="py-2 text-sm text-gray-700 dark:text-gray-200"
-                                aria-labelledby="dropdownInformdropdownAvatarNameButtonationButton">
-                                <li>
-                                    <a href="{{ route('profile.edit') }}"
-                                        class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Profile</a>
-                                </li>
-                                <li>
-                                    <a href="#"
-                                        class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
-                                </li>
-                                <li>
-                                    <a href="#"
-                                        class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
-                                </li>
-                            </ul>
-                            <div class="py-2">
-                                <a href="{{ route('logout') }}"
-                                    class="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
-                                    onclick="event.preventDefault(); this.closest('form').submit();">
-                                    Log out
-                                </a>
-                            </div>
-                        </div>
+					<!-- Dropdown menu -->
+					<div
+						class="z-10 hidden w-44 divide-y divide-gray-100 rounded-lg bg-white shadow dark:divide-gray-600 dark:bg-gray-700"
+						id="dropdownAvatarName"
+					>
+						<div class="px-4 py-3 text-sm text-gray-900 dark:text-white">
+							<div class="font-medium">Pro User</div>
+							<div class="truncate">{{ Auth::user()->email }}</div>
+						</div>
+						<ul
+							aria-labelledby="dropdownInformdropdownAvatarNameButtonationButton"
+							class="py-2 text-sm text-gray-700 dark:text-gray-200"
+						>
+							<li>
+								<a
+									class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+									href="{{ route('profile.edit') }}"
+								>Profile</a>
+							</li>
+							<li>
+								<a
+									class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+									href="#"
+								>Settings</a>
+							</li>
+							<li>
+								<a
+									class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+									href="#"
+								>Earnings</a>
+							</li>
+						</ul>
+						<div class="py-2">
+							<a
+								class="rounded-lg px-4 text-sm font-medium text-gray-800 hover:bg-gray-50 focus:outline-none focus:ring-4 focus:ring-gray-300 dark:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+								href="{{ route('logout') }}"
+								onclick="event.preventDefault(); this.closest('form').submit();"
+							>
+								Log out
+							</a>
+						</div>
+					</div>
+				</form>
+			@else
+				<div class="flex md:order-2">
+					<a
+						class="mr-3 space-x-7 rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 md:mr-0"
+						data-modal-target="authentication-modal"
+						data-modal-toggle="authentication-modal"
+						href="#"
+					>
+						Log in
+					</a>
+					<button
+						aria-controls="mobile-menu-2"
+						aria-expanded="false"
+						class="inline-flex items-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 md:hidden"
+						data-collapse-toggle="mobile-menu-2"
+						type="button"
+					>
+						<span class="sr-only">Open main menu</span>
+						<svg
+							aria-hidden="true"
+							class="h-6 w-6"
+							fill="currentColor"
+							viewBox="0 0 20 20"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<path
+								clip-rule="evenodd"
+								d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+								fill-rule="evenodd"
+							></path>
+						</svg>
+					</button>
+				</div>
+			@endauth
 
-                    </form>
-                @else
-                    <a href="#" data-modal-target="authentication-modal" data-modal-toggle="authentication-modal"
-                        class="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">
-                        Log in
-                    </a>
-                @endauth
-                <button data-collapse-toggle="mobile-menu-2" type="button"
-                    class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                    aria-controls="mobile-menu-2" aria-expanded="false">
-                    <span class="sr-only">Open main menu</span>
-                    <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd"
-                            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                            clip-rule="evenodd"></path>
-                    </svg>
-                    <svg class="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20"
-                        xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd"
-                            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                            clip-rule="evenodd"></path>
-                    </svg>
-                </button>
-                {{-- Light / Dark mode --}}
-                <button id="theme-toggle" type="button"
-                    class="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5">
-                    <svg id="theme-toggle-dark-icon" class="w-5 h-5 hidden" fill="currentColor" viewBox="0 0 20 20"
-                        xmlns="http://www.w3.org/2000/svg">
-                        <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
-                    </svg>
-                    <svg id="theme-toggle-light-icon" class="w-5 h-5 hidden" fill="currentColor" viewBox="0 0 20 20"
-                        xmlns="http://www.w3.org/2000/svg">
-                        <path
-                            d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
-                            fill-rule="evenodd" clip-rule="evenodd"></path>
-                    </svg>
-                </button>
-            </div>
-            <div class="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
-                <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-                    <li>
-                        <a href="#"
-                            class="block py-2 pr-4 pl-3 text-gray-400 rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white"
-                            aria-current="page">Home</a>
-                    </li>
-                    <li>
-                        <a href="#"
-                            class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Company</a>
-                    </li>
-                    <li>
-                        <a href="#"
-                            class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Marketplace</a>
-                    </li>
-                    <li>
-                        <a href="#"
-                            class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Features</a>
-                    </li>
-                    <li>
-                        <a href="#"
-                            class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Team</a>
-                    </li>
-                    <li>
-                        <a href="#"
-                            class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-</header>
+			{{-- Light / Dark mode --}}
+			<button
+				class="rounded-lg p-2.5 text-sm text-gray-500 hover:bg-inherit focus:outline-none focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-inherit dark:focus:ring-gray-700"
+				id="theme-toggle"
+				type="button"
+			>
+				<svg
+					class="hidden h-5 w-5"
+					fill="currentColor"
+					id="theme-toggle-dark-icon"
+					viewBox="0 0 20 20"
+					xmlns="http://www.w3.org/2000/svg"
+				>
+					<path
+						d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z">
+					</path>
+				</svg>
+				<svg
+					class="hidden h-5 w-5"
+					fill="currentColor"
+					id="theme-toggle-light-icon"
+					viewBox="0 0 20 20"
+					xmlns="http://www.w3.org/2000/svg"
+				>
+					<path
+						clip-rule="evenodd"
+						d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+						fill-rule="evenodd"
+					></path>
+				</svg>
+			</button>
+		</div>
+		<div
+			class="hidden w-full items-center justify-between md:order-1 md:flex md:w-auto"
+			id="mobile-menu-2"
+		>
+			<ul
+				class="mt-4 flex flex-col rounded-lg border border-gray-100 bg-gray-50 p-4 dark:border-gray-700 dark:bg-gray-800 md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:text-sm md:font-medium md:dark:bg-gray-900"
+			>
+				<li>
+					<a
+						aria-current="page"
+						class="block rounded-lg bg-blue-700 py-2 pl-3 pr-4 text-white dark:text-white md:bg-transparent md:p-0 md:text-blue-700"
+						href="#"
+					>Booking</a>
+				</li>
+				<li>
+					<a
+						class="block rounded-lg py-2 pl-3 pr-4 text-gray-700 hover:bg-gray-100 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white"
+						href="#"
+					>About Us</a>
+				</li>
+				<li>
+					<a
+						class="block rounded-lg py-2 pl-3 pr-4 text-gray-700 hover:bg-gray-100 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white"
+						href="#"
+					>Services</a>
+				</li>
+				<li>
+					<a
+						class="block rounded-lg py-2 pl-3 pr-4 text-gray-700 hover:bg-gray-100 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white"
+						href="#"
+					>Contact</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+</nav>
